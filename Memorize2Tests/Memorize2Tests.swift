6df110652b40c2fffc0d4b70568970d8f3382813
Jayden Neal <Jhneal17@gmail.com>
2024-12-24 12:30:32 -0500

    Initial Commit

---
+++ Memorize2Tests/Memorize2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize2Tests.swift
+//  Memorize2Tests
+//
+//  Created by jayden neal on 12/24/24.
+//
+
+import Testing
+@testable import Memorize2
+
+struct Memorize2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
