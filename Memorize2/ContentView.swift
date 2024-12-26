b3d04db74bf7f90e35fb6b5671be993fd8cddd91
Jayden Neal <Jhneal17@gmail.com>
2024-12-25 19:46:20 -0500

    Finished required task 1

--- Memorize2/ContentView.swift
+++ Memorize2/ContentView.swift
@@ -8,20 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
-    let emojis: Array<String> = ["👻","🎃","🕷","😈","🦇","🦈"] //Array<String> equivalent to [String], can also infer
-
+    let emojis: Array<String> = ["👻","🎃","🕷","😈","🦇","🦈","💩","👑","🎩","🕶","👓","🐶","🐱","🐭","🐹"]
+    
+    @State var cardCount: Int = 2
+    
+    //goal is keep structs below 12 lines most of the time
     var body: some View { //some view sorta auto deects return type of view for example if there is text, it will return text
-        HStack {
-            
-            ForEach(emojis.indices, id:\.self) { index in
-                CardView(content:emojis[index])
-                
+        VStack {
+            ScrollView{
+                cards
             }
             
+            Spacer()
+            cardCountAdjusters
         }
-        .foregroundColor(.orange)
+        
         .padding()
     }
+    var cards: some View {
+        return LazyVGrid(columns: [GridItem(.adaptive(minimum: 120 ))]) { //return actually isnt needed if one line
+            ForEach(0..<cardCount, id:\.self) { index in
+                CardView(content:emojis[index])
+                    .aspectRatio(2/3,contentMode: .fit)
+            }
+            
+        }.foregroundColor(.orange) //this all is considered one line
+
+    }
+    //by is the external name, offset is the internal name, symbol is same for both
+    func cardCountAdjuster(by offset: Int, symbol:String) -> some View {
+        Button(action:{
+                cardCount += offset
+        }, label : {
+            Image(systemName: symbol)
+                .imageScale(.large)
+                .font(.largeTitle)
+        }).disabled(cardCount+offset < 1 || cardCount+offset > emojis.count)
+    }
+    
+    var cardRemover: some View {
+        return cardCountAdjuster(by: -1, symbol: "rectangle.stack.badge.minus.fill")
+    }
+    var cardAdder: some View {
+        return cardCountAdjuster(by: +1, symbol: "rectangle.stack.badge.plus.fill")
+    }
+    var cardCountAdjusters: some View {
+        HStack {
+            cardRemover
+            Spacer()
+            cardAdder
+        }
+    }
+    
 }
 
 struct CardView: View {
@@ -30,13 +68,14 @@ struct CardView: View {
     var body: some View {
         ZStack(){ // vertical stack
             let base = RoundedRectangle(cornerRadius: 12) //let means constant
-            if (isFaceUp) {
+            Group {
                 base.fill(.white)
                 base.strokeBorder(lineWidth: 2)
-                Text(content)
-            } else {
-                RoundedRectangle(cornerRadius: 12) //.fill is default
+                Text(content).font(.largeTitle)
             }
+            .opacity(isFaceUp ? 1 : 0)
+            base.fill().opacity(isFaceUp ? 0 : 1)
+            
         }
         .onTapGesture {
             print("Tapped")
