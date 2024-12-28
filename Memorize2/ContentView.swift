b0786d7c06ddd42846c346c918b2c89d24afe375
Jayden Neal <Jhneal17@gmail.com>
2024-12-27 20:01:32 -0500

    Assignment 1

--- Memorize2/ContentView.swift
+++ Memorize2/ContentView.swift
@@ -7,99 +7,104 @@
 
 import SwiftUI
 
+enum Themes {
+    case halloween
+    case winter
+    case summer
+}
+let emojisHalloween: Array<String> = ["👻","🎃","🕷","😈","🦇","🦈","💩","👑","🎩","🕶","👓","🐶","🐱","🐭","🐹"]
+let emojisWinter: Array<String> = ["❄️","⛄️","🎄","🎅","🤶","🦌","🎁","🕯","🧊","🧤","🧣","🧥","🧦","🎿"]
+let emojisSummer: Array<String> = ["🌞","🌴","🌊","🏄","🏖","🍉","🍦","🍹","🏝","🏜"]
+
 struct ContentView: View {
-    let emojis: Array<String> = ["👻","🎃","🕷","😈","🦇","🦈","💩","👑","🎩","🕶","👓","🐶","🐱","🐭","🐹"]
+    @State var themeState: Themes = .halloween //This manages state, alternatively could of had separate emoji and color states
     
-    @State var cardCount: Int = 2
-    
-    //goal is keep structs below 12 lines most of the time
     var body: some View { //some view sorta auto deects return type of view for example if there is text, it will return text
         VStack {
-            ScrollView{
-                cards
+            Text("Memorize!").font(.largeTitle) //Title
+            ScrollView{ //Scrollable cards
+                cards //cards view
             }
-            
-            Spacer()
-            cardCountAdjusters
-        }
-        
-        .padding()
+            Spacer() //spacer to move theme changer to bottom
+            themeChanger() //theme change buttons
+                
+        }.padding() //default padding
     }
+    
     var cards: some View {
-        return LazyVGrid(columns: [GridItem(.adaptive(minimum: 120 ))]) { //return actually isnt needed if one line
-            ForEach(0..<cardCount, id:\.self) { index in
-                CardView(content:emojis[index])
-                    .aspectRatio(2/3,contentMode: .fit)
-            }
+        let (emojis,cardColor) = switch themeState {
+            case .halloween:
+                (emojisHalloween,Color.orange)
+            case .winter:
+                (emojisWinter, Color.blue)
+            case .summer:
+                (emojisSummer, Color.green)
+        }
+        return LazyVGrid(columns: [GridItem(.adaptive(minimum: 80 ))]) { //return actually isnt needed if one line
             
-        }.foregroundColor(.orange) //this all is considered one line
-
+            let shuffledEmojis = emojis.shuffled() // shuffle the deck
+            ForEach(0..<shuffledEmojis.count, id:\.self) { index in
+                CardView(content:shuffledEmojis[index]).aspectRatio(2/3,contentMode: .fit)
+            }
+        }.foregroundColor(cardColor) //this all is considered one line
     }
-    //by is the external name, offset is the internal name, symbol is same for both
-    func cardCountAdjuster(by offset: Int, symbol:String) -> some View {
-        Button(action:{
-                cardCount += offset
-        }, label : {
-            Image(systemName: symbol)
-                .imageScale(.large)
-                .font(.largeTitle)
-        }).disabled(cardCount+offset < 1 || cardCount+offset > emojis.count)
-    }
-    
-    var cardRemover: some View {
-        return cardCountAdjuster(by: -1, symbol: "rectangle.stack.badge.minus.fill")
-    }
-    var cardAdder: some View {
-        return cardCountAdjuster(by: +1, symbol: "rectangle.stack.badge.plus.fill")
-    }
-    var cardCountAdjusters: some View {
-        HStack {
-            cardRemover
+    func themeChanger() -> some View {
+        return HStack {
+            //Make button for all available themes
+            themeButton(.halloween)
             Spacer()
-            cardAdder
-        }
-    }
-    
-}
-
-struct CardView: View {
-    let content: String
-    @State var isFaceUp: Bool = false
-    var body: some View {
-        ZStack(){ // vertical stack
-            let base = RoundedRectangle(cornerRadius: 12) //let means constant
-            Group {
-                base.fill(.white)
-                base.strokeBorder(lineWidth: 2)
-                Text(content).font(.largeTitle)
-            }
-            .opacity(isFaceUp ? 1 : 0)
-            base.fill().opacity(isFaceUp ? 0 : 1)
+            themeButton(.summer)
+            Spacer()
+            themeButton(.winter)
             
         }
-        .onTapGesture {
-            print("Tapped")
-            //self view cannot be changed (its immutable) so isFaceUp = !isFaceUp wont work
-            //For temporary state can use @State in front to create a pointer to a state instead
-            isFaceUp = !isFaceUp
-        }
     }
+    func themeButton(_ themeToChangeTo: Themes)-> some View {
+        let isCurrentTheme = themeState == themeToChangeTo
+        let (label,symbol: symbol) = switch themeToChangeTo { //tuple so I can add a image too
+        case .halloween:
+            ("Halloween", symbol: "exclamationmark.triangle.fill")
+        case .summer:
+            ("Summer","sun.max")
+        case .winter:
+            ("Winter","snowflake")
+            
+        }
+        return Button(action: {
+            themeState = themeToChangeTo
+        }, label: {
+            VStack {
+                Image(systemName: symbol)
+                Text(label)
+            }
+        }
+        ).foregroundColor(isCurrentTheme ? .orange : .blue).font(.headline)
+    }
+    
+    struct CardView: View {
+        let content: String
+        @State var isFaceUp: Bool = false
         
+        var body: some View {
+            ZStack(){ // vertical stack
+                let base = RoundedRectangle(cornerRadius: 12) //let means constant
+                Group {
+                    base.fill(.white)
+                    base.strokeBorder(lineWidth: 2)
+                    Text(content).font(.largeTitle)
+                }
+                .opacity(isFaceUp ? 1 : 0)
+                base.fill().opacity(isFaceUp ? 0 : 1)
+                
+            }
+            .onTapGesture {
+                print("Tapped")
+                isFaceUp = !isFaceUp //toggle
+            }
+        }
+        
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 #Preview {
     ContentView()
